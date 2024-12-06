d0973de2676d860f893f60ec22a239273b04cb88
TheSteff5 <43687485+TheSteff5@users.noreply.github.com>
2024-12-06 16:06:14 +0100

    Issue-ID: B - Implemented responsive behavior for header and footer, added sidenav if phone view

--- internship-frontend/src/app/components/pages/landing-page/landing-page.component.ts
+++ internship-frontend/src/app/components/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Contact } from '../../../model/Contact';
 import { CommonModule } from '@angular/common';
@@ -16,8 +16,12 @@ import { FormsModule } from '@angular/forms';
 })
 export class LandingPageComponent implements OnInit {
   @ViewChild(GridComponentComponent) gridElement!: GridComponentComponent;
-  searchValue: string = '';
+  @ViewChild('wrapper') wrapper!: ElementRef; /* wrapper for sidenav navigation */
+  @ViewChild('sidenav') sidenav!: ElementRef;
 
+  pokemonData: PokemonData[] = [];
+  isPhone = false;
+  searchValue: string = '';
   navigationItems: string[] = ["nav-1", "nav-2", "nav-3"];
   contactInformation: Contact = {
     name: "Stefan Miskovic",
@@ -26,11 +30,16 @@ export class LandingPageComponent implements OnInit {
     email: "stefanmiskovic11@hotmail.com"
   };
 
-  pokemonData: PokemonData[] = [];
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.isPhone = window.innerWidth <= 768;
+    console.log(this.isPhone)
+  }
 
   constructor(private pokeService: PokeService, private searchService: SearchService) { }
 
   ngOnInit(): void {
+    this.onResize();
     this.pokeService.getPokemonListWithImages().subscribe(res => {
       this.pokemonData = res;
     });
@@ -38,5 +47,19 @@ export class LandingPageComponent implements OnInit {
 
   onValueChange(searchValue: string) {
     this.searchService.filterItems(this.gridElement.el, searchValue);
+  }
+
+  openSideNav() {
+    this.wrapper.nativeElement.style.width = '100%';
+    this.sidenav.nativeElement.style.width = '60%';
+  }
+
+  closeSideNav() {
+    this.wrapper.nativeElement.style.width = '0';
+    this.sidenav.nativeElement.style.width = '0';
+  }
+
+  onSidenavClick(event: MouseEvent) {
+    event.stopPropagation(); /* Prevent event to be executed if clicked inside the sidenav content */
   }
 }
