2d18b5d199084ed56f428dceb2eaa4de8ad807c9
TheSteff5 <43687485+TheSteff5@users.noreply.github.com>
2024-12-06 03:06:42 +0100

    Issue-ID: E - Added searchService and filter for grid

--- internship-frontend/src/app/components/pages/landing-page/landing-page.component.ts
+++ internship-frontend/src/app/components/pages/landing-page/landing-page.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Contact } from '../../../model/Contact';
 import { CommonModule } from '@angular/common';
 import { GridComponentComponent } from "../../utils/grid-component/grid-component.component";
 import { PokeService } from '../../../services/poke.service';
 import { PokemonData } from '../../../model/Pokemon';
+import { SearchService } from '../../../services/search.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [CommonModule, RouterLink, GridComponentComponent],
+  imports: [CommonModule, RouterLink, GridComponentComponent, FormsModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent implements OnInit {
+  @ViewChild(GridComponentComponent) gridElement!: GridComponentComponent;
+  searchValue: string = '';
+
   navigationItems: string[] = ["nav-1", "nav-2", "nav-3"];
   contactInformation: Contact = {
     name: "John",
@@ -23,13 +28,17 @@ export class LandingPageComponent implements OnInit {
 
   pokemonData: PokemonData[] = [];
 
-  constructor(private pokeService: PokeService) {
+  constructor(private pokeService: PokeService, private searchService: SearchService) { }
 
-  }
-
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.pokeService.getPokemonListWithImages().subscribe(res => {
       this.pokemonData = res;
     });
+  }
+
+  onValueChange(searchValue: string) {
+
+    this.searchService.filterItems(this.gridElement.el, searchValue);
+
   }
 }
